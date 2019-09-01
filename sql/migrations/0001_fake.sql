06d5e5e5f7f6f1c9d7705c4a63a933796f3fee71
Kyle Johnson <negatecx@gmail.com>
2019-08-31 23:13:22 -0600

    Cleaning up bogus test files

--- sql/migrations/0001_fake.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-SELECT _v.register_patch('0001_fake');
-
-ALTER TABLE team ADD COLUMN fake_column INT;
-
-COMMIT;
--- sql/rollbacks/0001_fake.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-SELECT _v.unregister_patch('0001_fake');
-
-ALTER TABLE team DROP COLUMN fake_column;
-
-COMMIT;
