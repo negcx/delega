afa6215c065be6ffef85e9dcc1b12b9561ec1452
Kyle Johnson <negatecx@gmail.com>
2019-08-31 22:49:24 -0600

    Fix migrate script.

--- sql/migrate.sh
+++ sql/migrate.sh
@@ -6,7 +6,7 @@ DB_NAME=${1:-$DATABASE_URL}
 
 if [ "$DB_NAME" != "" ]; then
     while read p; do
-        source $DIR/migrate_one.sh $1 $p
+        source $DIR/migrate_one.sh $DB_NAME $p
     done < $DIR/migrations.txt
 else
     echo "Missing required parameter: database name."
