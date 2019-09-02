f280ffcd09353c94083765b6b0d1e671b44b9619
Kyle Johnson <negatecx@gmail.com>
2019-09-02 12:01:28 -0600

    Setup PostCSS, Tailwind, and PurgeCSS and linked them into Webpack.

---
+++ assets/postcss.config.js
@@ -0,0 +1,8 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["../lib/delega_web/templates/**/*.html.eex"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer"), purgecss]
+};
