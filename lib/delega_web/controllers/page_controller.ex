0a52495be51382a6c8f4eead45f22ab8f20793f8
Kyle Johnson <negatecx@gmail.com>
2019-09-12 21:53:40 -0600

    loader.io

--- lib/delega_web/controllers/page_controller.ex
+++ lib/delega_web/controllers/page_controller.ex
@@ -11,6 +11,10 @@ defmodule DelegaWeb.PageController do
     render(conn, "oauth_failure.html", client_id: @client_id)
   end
 
+  def loader_io(conn, _params) do
+    conn |> send_resp(200, "loaderio-5c5817b4751c09eb622cccc517a55ff5")
+  end
+
   def oauth_success(conn, _params) do
     render(conn, "oauth_success.html")
   end
